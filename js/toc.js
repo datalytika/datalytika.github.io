a32b8d97e422c584c15c52cb189fe6c6d4351548
Joseph Lweya <84285814+lweyajoe@users.noreply.github.com>
2024-03-11 10:19:46 +0300

    Update toc.js

--- js/toc.js
+++ js/toc.js
@@ -1,7 +1,7 @@
 var ToC =
   "<div class='section-title section-title-sm position-relative pb-3 mb-4'>" +
     "<h1>On this page:</h1>" +
-    "<ul class='link-animated d-flex flex-column justify-content-start h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2'>";
+    "<ul>";
 
 var newLine, el, title, link;
 
@@ -12,7 +12,7 @@ $("article h1").each(function() {
   link = "#" + el.attr("id");
 
   newLine =
-    "<li>" +
+    "<li class='link-animated d-flex flex-column justify-content-start h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2'>" +
       "<a href='" + link + "'>" +
         title +
       "</a>" +
